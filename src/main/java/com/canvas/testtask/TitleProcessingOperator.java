34cdb6de8f3f8f0e7de197a9532783c333de837f
mitrixi <balalindmitry@gmail.com>
2022-06-30 01:11:01 +0300

    resources initialization fix

--- src/main/java/com/canvas/testtask/TitleProcessingOperator.java
+++ src/main/java/com/canvas/testtask/TitleProcessingOperator.java
@@ -1,17 +1,16 @@
 package com.canvas.testtask;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TitleProcessingOperator {
 
     public static String shortenTitleLength(int requiredLength, String title) {
-        StringBuilder finalTitle = new StringBuilder();
-        Matcher matcher = Pattern.compile("(\\S+)").matcher(title);
-
         if (title.length() <= requiredLength) {
             return title;
         } else {
+            var matcher = Pattern.compile("(\\S+)").matcher(title);
+            var finalTitle = new StringBuilder();
+
             while (matcher.find() && finalTitle.length() + matcher.group().length() <= requiredLength - 3) {
                 finalTitle.append(matcher.group()).append(" ");
             }
